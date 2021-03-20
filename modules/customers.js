2f205bfa8948c9b0f7a1b5ea02542eec8c0a28dd
rfranu7 <57077484+rfranu7@users.noreply.github.com>
2021-03-20 09:28:49 +0800

    working on enroll customer post data endpoint

--- modules/customers.js
+++ modules/customers.js
@@ -5,11 +5,13 @@ export default class Customer {
 
     async enrollCustomer(first_name, last_name, email_address, account_status, callback) {
         var sql = `INSERT INTO ${tableName} (first_name, last_name, email_address, account_status) VALUES (${first_name}, ${last_name}, ${email_address}, ${account_status})`;
+        console.log(sql);
         dbConnect(sql, callback);
     }
 
     async findCustomerByEmail(email_address, callback) {
         var sql = `SELECT email_address FROM ${tableName} WHERE email_address = ${email_address}`;
+        console.log(sql);
         dbConnect(sql, callback);
     }
 }
